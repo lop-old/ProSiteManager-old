1b52899411f9a7ee0d34c5bfabdd4d78ad6df41a
lorenzo42p@gmail.com <lorenzo42p@gmail.com@e374cdb8-7ee8-0d77-fd1e-c554042f9154>
2013-02-28 10:13:03 +0000

    added salt from end and salt using custom string   added setSalt() function
    git-svn-id: http://prositemanager.googlecode.com/svn/trunk@57 e374cdb8-7ee8-0d77-fd1e-c554042f9154

--- portal/classes/Utils/PassCrypt.class.php
+++ portal/classes/Utils/PassCrypt.class.php
@@ -1,37 +1,50 @@
 <?php namespace psm\Utils;
 if(!defined('PORTAL_INDEX_FILE') || \PORTAL_INDEX_FILE!==TRUE){if(headers_sent()){echo '<header><meta http-equiv="refresh" content="0;url=../"></header>';}else{header('HTTP/1.0 301 Moved Permanently'); header('Location: ../');} die("<font size=+2>Access Denied!!</font>");}
 //methods:
-// md5
-// rev
-// splitrev
-// revsplit
-// salt:#
-// loop:#:mode
+// md5				- standard md5
+// rev				- reverse the hash/string
+// splitrev:x		- reverse last x chars
+// revsplit:x		- reverse first x chars
+// salt:#x			- salt using the first x chars
+// salt:#-x			- salt using the last x chars
+// salt:string		- salt using custom string
+// loop:x:action	- loop action x times
+//TODO: loopasc:x:action	- loop by the value
 //examples:
 // 'md5'
-//   - standard md5() only
-// 'md5 salt:6 md5'
-//   - md5(), salt using first 6 chars, md5() once more
-// 'loop:1000:md5 revsplit:10 salt:6 loop:1000:md5 splitrev:22 loop:500:md5'
-//   - md5() x 1000, reverse first 10 chars, salt using first 6 chars,
-//     md5() x 1000, reverse last 10 chars, md5() x 500
+//    - standard md5 only
+// 'md5 salt:{salt} md5'
+//    - md5, salt using a custom string, then md5 once more
+// 'loop:1000:md5 revsplit:10 salt:#6 salt:#-6 loop:1000:md5 salt:{salt} loop:1000:md5 splitrev:22 loop:500:md5';
+//    - md5 x 1000, reverse first 10 chars, salt using first 6 chars, salt using last 6 chars,
+//      md5 x 1000, salt using a custom string, md5 x 1000, reverse last 10 chars, md5 x 500
 
 
 class PassCrypt {
 
-	const defaultHashSequence = 'loop:1000:md5 revsplit:10 salt:6 loop:1000:md5 splitrev:22 loop:500:md5';
+	const defaultHashSequence = 'loop:1000:md5 revsplit:10 salt:#6 salt:#-6 loop:1000:md5 salt:{salt} loop:1000:md5 splitrev:22 loop:500:md5';
+	private $salt = NULL;
 	private $hashSequence = '';
 
 
-	public function __construct($hashSequence='') {
+	public function __construct($hashSequence='', $salt='') {
 		if(!empty($hashSequence))
 			$this->hashSequence = $hashSequence;
+		if(!empty($salt))
+			$this->salt = $salt;
+	}
+
+
+	// set salt (only once)
+	public function setSalt($salt) {
+		if($this->salt != NULL) return;
+		$this->salt = $salt;
 	}
 
 
 	// single use hash
-	public static function hashNow($data, $hashSequence='') {
-		$crypt = new self($hashSequence);
+	public static function hashNow($data, $hashSequence='', $salt='') {
+		$crypt = new self($hashSequence, $salt);
 		$output = $crypt->hash($data);
 		unset($crypt);
 		return $output;
@@ -48,12 +61,14 @@ class PassCrypt {
 		foreach($modes as $hashMode) {
 			$hashMode = trim($hashMode);
 			if(empty($hashMode)) continue;
-			self::_hashThis($data, $hashMode);
+			self::_hashThis($data, $hashMode, $this->salt);
 		}
 		return $data;
 	}
-	private static function _hashThis(&$data, $mode) {
+	private static function _hashThis(&$data, $mode, $salt) {
 		@list($mode, $arg) = explode(':', $mode, 2);
+		if(!empty($salt))
+			$mode = str_replace('{salt}', $this->salt, $mode);
 		$mode = trim($mode);
 		$arg  = trim($arg);
 		// md5
@@ -84,16 +99,35 @@ class PassCrypt {
 		}
 		// salt
 		if($mode == 'salt') {
-			$data =
-				substr($data, 0, ((int) $arg) ).
-				':'.
-				$data;
+			// salt self length
+			if(Utils_Strings::startsWith($arg, '#')) {
+				$arg = (int) substr($arg, 1);
+				// from front of hash
+				if($arg > 0)
+					$data =
+						substr($data, 0, $arg).
+						':'.
+						$data;
+				// from end of hash
+				else
+					$data =
+						$data.
+						':'.
+						substr($data, $arg);
+			// salt string
+			} else {
+				$data =
+					trim($arg).
+					':'.
+					$data;
+			}
 			return;
 		}
 		// loop
 		if($mode == 'loop') {
 			$parts = explode(':', $arg, 2);
-			for($i=0; $i< ((int) $parts[0]); $i++)
+			$count = (int) $parts[0];
+			for($i=0; $i < $count; $i++)
 				self::_hashThis($data, $parts[1]);
 			return;
 		}

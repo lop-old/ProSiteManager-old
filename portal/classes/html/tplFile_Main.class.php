ababc890944dfc9fe8108ad910440b48c3c4367b
lorenzo42p@gmail.com <lorenzo42p@gmail.com@e374cdb8-7ee8-0d77-fd1e-c554042f9154>
2013-03-02 13:05:42 +0000

    renamed File and File_Main classes to tplFile and tplFile_Main
    git-svn-id: http://prositemanager.googlecode.com/svn/trunk@60 e374cdb8-7ee8-0d77-fd1e-c554042f9154

--- portal/classes/html/tplFile_Main.class.php
+++ portal/classes/html/tplFile_Main.class.php
@@ -1,6 +1,6 @@
 <?php namespace psm\html;
 if(!defined('PORTAL_INDEX_FILE') || \PORTAL_INDEX_FILE!==TRUE){if(headers_sent()){echo '<header><meta http-equiv="refresh" content="0;url=../"></header>';}else{header('HTTP/1.0 301 Moved Permanently'); header('Location: ../');} die("<font size=+2>Access Denied!!</font>");}
-class File_Main extends \psm\html\File {
+class tplFile_Main extends tplFile {
 
 	private static $htmlMain = NULL;
 

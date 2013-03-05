93e789b7746fd7b2822f697bad74142d91aee1cc
lorenzo42p@gmail.com <lorenzo42p@gmail.com@e374cdb8-7ee8-0d77-fd1e-c554042f9154>
2013-03-05 06:26:15 +0000

    moving NavBar and DataTables classes into Widgets folder/namespace
    git-svn-id: http://prositemanager.googlecode.com/svn/trunk@71 e374cdb8-7ee8-0d77-fd1e-c554042f9154

--- portal/classes/Widgets/DataTables/Table.class.php
+++ portal/classes/Widgets/DataTables/Table.class.php
@@ -1,4 +1,4 @@
-<?php namespace psm\DataTables;
+<?php namespace psm\Widgets\DataTables;
 if(!defined('psm\INDEX_FILE') || \psm\INDEX_FILE!==TRUE) {if(headers_sent()) {echo '<header><meta http-equiv="refresh" content="0;url=../"></header>';} else {header('HTTP/1.0 301 Moved Permanently'); header('Location: ../');} die("<font size=+2>Access Denied!!</font>");}
 class Table {
 

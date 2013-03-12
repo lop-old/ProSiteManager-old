21cc3d16e312edaef382ba7ca83d8a4f32424f2a
lorenzo42p@gmail.com <lorenzo42p@gmail.com@e374cdb8-7ee8-0d77-fd1e-c554042f9154>
2013-03-12 12:58:10 +0000

    updated default index.php to work with new framework changes   the rest of the testportal module is probably still broken
    git-svn-id: http://prositemanager.googlecode.com/svn/trunk@92 e374cdb8-7ee8-0d77-fd1e-c554042f9154

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
+define('psm\DEBUG',          TRUE);
+//define('psm\DEFAULT_MODULE', 'testportal');
+//define('psm\DEFAULT_PAGE',   'home');
+
 // load the portal
-include('portal/PortalLoader.php');
-\psm\newPortal('testportal');
+include(__DIR__.'/portal/Portal.php');
+$portal = \psm\Portal::factory();
 
 ?>

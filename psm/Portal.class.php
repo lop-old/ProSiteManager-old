b127514ba436802d493fd8966fda35086bd29fbd
lorenzo42p@gmail.com <lorenzo42p@gmail.com@e374cdb8-7ee8-0d77-fd1e-c554042f9154>
2013-04-16 06:56:28 +0000

    removed INDEX_FILE define check from classes where not really needed   all code in these files are inside classes which can't be run directly
    git-svn-id: http://prositemanager.googlecode.com/svn/trunk@134 e374cdb8-7ee8-0d77-fd1e-c554042f9154

--- psm/Portal.class.php
+++ psm/Portal.class.php
@@ -2,8 +2,7 @@
 // PSM - Content Management Framework
 // (c) (t) 2004-2013
 // Mattsoft.net PoiXson.com
-if(!defined('psm\\INDEX_FILE') || \psm\INDEX_FILE!==TRUE) {if(headers_sent()) {echo '<header><meta http-equiv="refresh" content="0;url=../"></header>';}
-	else {header('HTTP/1.0 301 Moved Permanently'); header('Location: ../');} die('<font size="+2">Access Denied!!</font>');}
+
 use \psm\Utils as Utils;
 global $ClassCount; $ClassCount++;
 // portal core
